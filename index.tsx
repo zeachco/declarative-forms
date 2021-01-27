cf719b09dd71e80811828c652326a31f47620783
Olivier Rousseau-Guyot <olivier.rousseauguyot@shopify.com>
2021-01-27 13:57:09 -0500

    moved demo root to demo.tsx

--- index.tsx
+++ index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { render } from 'react-dom';
 import './style.css';
-import { App } from './demo';
+import { App } from './demo/demo';
 
 render(<App />, document.getElementById('root'));

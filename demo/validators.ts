89f112f70359712461bc98c7daed9b52875e521b
Olivier Rousseau-Guyot <olivier.rousseauguyot@shopify.com>
2021-01-27 12:03:51 -0500

    fixed validator format with new options

--- demo/validators.ts
+++ demo/validators.ts
@@ -4,12 +4,22 @@ export function PresenceValidator(val: any, _options: Validator) {
   return Boolean(val) ? '' : 'PresenceError :: Field must be defined';
 }
 
+function validateRegex(val: any, format: string) {
+  const exp = new RegExp(format);
+  return exp.test(val)
+    ? ''
+    : `FormatError :: Field does not match expression ${format}`;
+}
+
 export function FormatValidator(val: any, options: Validator) {
   if (!options.format) {
     return '';
   }
-  const exp = new RegExp(options.format);
-  return exp.test(val)
-    ? ''
-    : `FormatError :: Field does not match expression ${options.format}`;
+
+  if (typeof options.format === 'string') {
+    return validateRegex(val, options.format);
+  }
+
+  console.warn('unsupported options in FormatValidator');
+  return '';
 }

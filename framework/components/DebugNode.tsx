5103eb23da6ba6404efb4a933f35e44eeb8c9366
Olivier Rousseau-Guyot <olivier.rousseauguyot@shopify.com>
2021-01-29 09:09:35 -0500

    debug style

--- framework/components/DebugNode.tsx
+++ framework/components/DebugNode.tsx
@@ -12,12 +12,14 @@ export function DebugNode({ children, node, name = '' }: DebugProps) {
   const style = {
     boxShadow: `inset 0 0 3px 1px #${color}, inset -.5em 0 2em -.5em #${color}44`,
     backgroundColor: `#${color}08`,
-    padding: '.1em 0 .1em .3em',
+    padding: '.1em 0 .1em .6em',
+    margin: '.2em',
+    borderRadius: '.5em',
   };
   return (
     <div key={'debug_' + node.path} title={name} style={style}>
       <small style={{ color }}>
-        &lt;{name} path="{node.path}" kind="{node.schema.kind}" &gt;
+        &lt;{name} path="{node.path}" &gt;
       </small>
       {children}
       <small style={{ color }}>&lt;/{name} &gt;</small>

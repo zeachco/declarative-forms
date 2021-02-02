a6f9f99610dfeeccc588420d9247c8a601d2981c
Olivier Rousseau-Guyot <olivier.rousseauguyot@shopify.com>
2021-02-02 16:18:02 -0500

    fix list node for framework

--- framework/components/ListNode.tsx
+++ framework/components/ListNode.tsx
@@ -1,19 +1,25 @@
 import React from 'react';
 
-import {useNode} from '../utilities/hook';
-import {SchemaNode} from '../SchemaNode';
+import { useNode } from '../utilities/hook';
+import { SchemaNode } from '../SchemaNode';
 
-import {NodeProps, RootNode} from './RootNode';
+import { NodeProps, RootNode } from './RootNode';
 
-export function ListNode({node, context}: NodeProps) {
-  const {errors, addListItem, removeListItem} = useNode(node);
+export function ListNode({ node, context }: NodeProps) {
+  const { errors, addListItem } = useNode(node);
 
   const jsx: React.ReactNodeArray = [];
+  // HACK deleting a node < max length shift the dom elements and cause fields to be irresponsive
+  const uid = Math.random();
 
-  node.value.forEach((subNode: SchemaNode, index: number) => {
+  node.value.forEach((subNode: SchemaNode) => {
     jsx.push(
-      <div>
-        <button type="button" style={{float: 'right'}} onClick={subNode.deleteSelf}>
+      <div key={uid}>
+        <button
+          type="button"
+          style={{ float: 'right' }}
+          onClick={subNode.deleteSelf}
+        >
           X
         </button>
         <RootNode key={subNode.uid} context={context} node={subNode} />

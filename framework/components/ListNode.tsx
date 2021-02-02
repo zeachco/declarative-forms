e22cac28de142a8aa3ab676c165aed96a52b4195
Olivier Rousseau-Guyot <olivier.rousseauguyot@shopify.com>
2021-02-02 16:07:00 -0500

    fixed framework list component

--- framework/components/ListNode.tsx
+++ framework/components/ListNode.tsx
@@ -6,22 +6,18 @@ import {SchemaNode} from '../SchemaNode';
 import {NodeProps, RootNode} from './RootNode';
 
 export function ListNode({node, context}: NodeProps) {
-  const {validate, errors} = useNode(node);
+  const {errors, addListItem, removeListItem} = useNode(node);
 
   const jsx: React.ReactNodeArray = [];
 
   node.value.forEach((subNode: SchemaNode, index: number) => {
-    const handleDelete = () => {
-      node.removeListItem(index);
-      validate();
-    };
     jsx.push(
       <div>
-        <button type="button" style={{float: 'right'}} onClick={handleDelete}>
+        <button type="button" style={{float: 'right'}} onClick={subNode.deleteSelf}>
           X
         </button>
         <RootNode key={subNode.uid} context={context} node={subNode} />
-      </div>,
+      </div>
     );
   });
 
@@ -32,14 +28,9 @@ export function ListNode({node, context}: NodeProps) {
         <strong key={err}>{node.translate(err, 'error')}</strong>
       ))}
       {jsx}
-      <button type="button" onClick={handleAdd}>
+      <button type="button" onClick={addListItem}>
         Add node
       </button>
     </div>
   );
-
-  function handleAdd() {
-    node.addListItem();
-    validate();
-  }
 }

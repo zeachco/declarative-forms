02f300cfdf01863a85c508489cc4ced56c5d958e
Olivier <zeachco@gmail.com>
2021-02-10 12:40:17 -0500

    construction performance

--- plugins/basic/index.ts
+++ plugins/basic/index.ts
@@ -4,17 +4,11 @@ import {StringNode} from './components/StringNode';
 import {PolymorphicNode} from './components/PolymorphicNode';
 
 export function decorateWithBasicComponents(context: FormContext) {
-  context.where(({schema}) => schema.type === 'string').replaceWith(StringNode);
-
-  context
-    .where(({schema}) => ['number', 'integer', 'float'].includes(schema.type))
-    .replaceWith(StringNode);
-
-  context
-    .where(({schema}) => schema.type === 'boolean')
-    .replaceWith(BooleanNode);
-
-  context
-    .where(({schema}) => schema.type === 'polymorphic')
-    .replaceWith(PolymorphicNode);
+  Object.assign(context.plugins, {
+    string: StringNode,
+    number: StringNode,
+    integer: StringNode,
+    boolean: BooleanNode,
+    polymorphic: PolymorphicNode,
+  });
 }
--- plugins/polaris/index.ts
+++ plugins/polaris/index.ts
@@ -4,15 +4,11 @@ import {PolarisPolymorphicNode} from './components/PolarisPolymorphicNode';
 import {PolarisStringNode} from './components/PolarisStringNode';
 
 export function decorateWithPolarisComponents(context: FormContext) {
-  context
-    .where(({schema}) => schema.type === 'string' || schema.type === 'integer')
-    .replaceWith(PolarisStringNode);
-
-  context
-    .where(({schema}) => schema.type === 'boolean')
-    .replaceWith(PolarisBooleanNode);
-
-  context
-    .where(({schema}) => schema.type === 'polymorphic')
-    .replaceWith(PolarisPolymorphicNode);
+  Object.assign(context.plugins, {
+    string: PolarisStringNode,
+    number: PolarisStringNode,
+    integer: PolarisStringNode,
+    boolean: PolarisBooleanNode,
+    polymorphic: PolarisPolymorphicNode,
+  });
 }

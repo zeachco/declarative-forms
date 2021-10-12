4e2fb21237cba1ee1f3fdec147b2f6b893c8b7ae
Olivier <zeachco@gmail.com>
2021-10-12 17:05:50 -0400

    1.0.0

--- __source/plugins/basic/components/index.ts
+++
@@ -1,5 +0,0 @@
-export {BooleanNode} from './BooleanNode';
-export {ListNode} from './ListNode';
-export {NumericNode} from './NumericNode';
-export {PolymorphicNode} from './PolymorphicNode';
-export {StringNode} from './StringNode';
--- __source/plugins/polaris/components/index.ts
+++
@@ -1,6 +0,0 @@
-export {PolarisBooleanNode} from './PolarisBooleanNode';
-export {PolarisPolymorphicNode} from './PolarisPolymorphicNode';
-export {PolarisRangeSlider} from './PolarisRangeSlider';
-export {PolarisStringNode} from './PolarisStringNode';
-export {PolarisLayoutGridPosition} from './PolarisLayoutGridPosition';
-export {PolarisOptionsNode} from './PolarisOptionsNode';
